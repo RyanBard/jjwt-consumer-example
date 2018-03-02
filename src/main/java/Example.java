986017b1fc3d37f53886fb479b27a02e39f02ffa
John Bard <jbard@vmware.com>
2018-03-02 10:07:29 -0500

    Adding tests to exercise more of the code
    Signed-off-by: John Bard <jbard@vmware.com>

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,11 +1,37 @@
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.impl.Base64Codec;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
 
 public class Example {
 
     public static String exerciseBase64Codec(String input) {
         Base64Codec codec = new Base64Codec();
         return new String(codec.decode(codec.encode(input.getBytes())));
+    }
+
+    public static String exerciseBuilder(String key, String subject) {
+        Base64Codec codec = new Base64Codec();
+        return Jwts.builder()
+                .setSubject(subject)
+                .signWith(SignatureAlgorithm.HS256, codec.encode(key.getBytes()))
+                .compact();
+    }
+
+    public static Claims exerciseParser(String keyStr, String compactStr) {
+        Base64Codec codec = new Base64Codec();
+        Key key = new SecretKeySpec(
+                keyStr.getBytes(),
+                SignatureAlgorithm.HS256.getJcaName()
+        );
+        return Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(compactStr)
+                .getBody();
     }
 
     public static void main(String[] args) {
--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -11,4 +11,20 @@ public class ExampleTest {
         assertEquals(input, Example.exerciseBase64Codec(input));
     }
 
+    @Test
+    public void testDefaultJwtBuilder() throws Exception {
+        String key = "abc123";
+        String subject = "Joe";
+        String expected = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJKb2UifQ.dpfYOKiEwcUXaok-UztzgvBh1HcKr3pqAFFTOJ3sbsU";
+        assertEquals(expected, Example.exerciseBuilder(key, subject));
+    }
+
+    @Test
+    public void testDefaultJwtParser() throws Exception {
+        String key = "abc123";
+        String compact = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJKb2UifQ.dpfYOKiEwcUXaok-UztzgvBh1HcKr3pqAFFTOJ3sbsU";
+        String expected = "Joe";
+        assertEquals(expected, Example.exerciseParser(key, compact).getSubject());
+    }
+
 }
